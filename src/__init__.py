811243dd5e3d859f4be5565975346d627c56a6dc
Rasmitha-S <rasmitha127@gmail.com>
2026-08-29 06:13:26 +0530

    fix: resolve MCU simulator import error

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,9 @@
 """
 PS09 TinyQuant Source Package.
 """
+from src.metrics import evaluate_memory_budget, simulate_mcu_resources
+
+__all__ = [
+    "evaluate_memory_budget",
+    "simulate_mcu_resources",
+]
